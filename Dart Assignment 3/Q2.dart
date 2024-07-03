2ec0304a695baa1cf9a3144fdb60de5d3ce6727c
Jawwad Ahmed <mjawwadahmed2021@gmail.com>
2024-07-03 19:59:12 +0500

    update Q2

--- Dart Assignment 3/Q2.dart
+++ Dart Assignment 3/Q2.dart
@@ -1,15 +1,18 @@
 import 'dart:io';
 
 void main() {
-  List fabonacci = [0, 1];
-  print('Enter number');
+  List<int> fabonacci = [0, 1];
+  print('Enter number of elements');
   int input = int.parse(stdin.readLineSync()!);
-  for (int i = 2; i < input - 3; i++) {
-    if (input == 0 || input == 1) {
-      fabonacci.add(1);
-    } else if (input >= 2) {
-      fabonacci.add(fabonacci[i - 1] + fabonacci[i - 2]);
-    }
+  if (input <= 0) {
+    print([]);
+    return;
+  } else if (input == 1) {
+    print([0]);
   }
+  for (int i = 2; i < input; i++) {
+    fabonacci.add(fabonacci[i - 1] + fabonacci[i - 2]);
+  }
+
   print(fabonacci);
 }
